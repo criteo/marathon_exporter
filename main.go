7922e0e936e8d5bcef3cc3eaccb26735aa19c2b7
Dylan Meissner <dylanmei@gmail.com>
2016-02-09 18:55:57 -0800

    Add connect-retry during startup

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
+	"net"
 	"net/http"
 	"net/url"
+	"time"
 
+	"github.com/gambol99/go-marathon"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/log"
 )
@@ -23,11 +27,53 @@ var (
 		"URI of Marathon")
 )
 
+func marathonConnect(uri *url.URL) error {
+	config := marathon.NewDefaultConfig()
+	config.URL = uri.String()
+	config.HTTPClient = &http.Client{
+		Timeout: 10 * time.Second,
+		Transport: &http.Transport{
+			Dial: (&net.Dialer{
+				Timeout: 10 * time.Second,
+			}).Dial,
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+
+	log.Debugln("Connecting to Marathon")
+	client, err := marathon.NewClient(config)
+	if err != nil {
+		return err
+	}
+
+	info, err := client.Info()
+	if err != nil {
+		return err
+	}
+
+	log.Debugf("Connected to Marathon! Name=%s, Version=%s\n", info.Name, info.Version)
+	return nil
+}
+
 func main() {
 	flag.Parse()
 	uri, err := url.Parse(*marathonUri)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	retryTimeout := time.Duration(10 * time.Second)
+	for {
+		err := marathonConnect(uri)
+		if err == nil {
+			break
+		}
+
+		log.Debugf("Problem connecting to Marathon: %v", err)
+		log.Infof("Couldn't connect to Marathon! Trying again in %v", retryTimeout)
+		time.Sleep(retryTimeout)
 	}
 
 	exporter := NewExporter(uri)
