bd6b9235198469d9cab6359d1ca0cfbe0cbad55e
sober <ws1992jx@163.com>
2020-09-19 14:14:05 +0800

    fix: metrics data not show

--- scraper.go
+++ scraper.go
@@ -31,11 +31,17 @@ func (s *scraper) Scrape(path string) ([]byte, error) {
 		},
 	}
 
-	response, err := client.Get(fmt.Sprintf("%v/%s", s.uri, path))
+	//response, err := client.Get(fmt.Sprintf("%v/%s", s.uri, path))
+	mURL := fmt.Sprintf("%v/%s", s.uri, path)
+	reqest, err := http.NewRequest("GET", mURL, nil)
 	if err != nil {
 		return nil, err
 	}
-
+	reqest.SetBasicAuth(*marathonUserName, *marathonPassword)
+	response, err := client.Do(reqest)
+	if err != nil {
+		return nil, err
+	}
 	defer response.Body.Close()
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
